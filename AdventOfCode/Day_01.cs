9526e9b994339448097cba20a8ad7c87cf148493
Sean Edwards <hello@seanedwards.dev>
2021-12-02 16:07:23 +0000

    Solutions for day 1 and 2

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -2,14 +2,38 @@
 
 public class Day_01 : BaseDay
 {
-    private readonly string _input;
+    private readonly int[] _input;
 
     public Day_01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllLines(InputFilePath).Select(int.Parse).ToArray();
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        var totalIncrements = 0;
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        for (var i = 1; i < _input.Length; i++)
+        {
+            totalIncrements += _input[i] > _input[i - 1] ? 1 : 0;
+        }
+
+        return new(totalIncrements.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var previousIncrement = 0;
+        var totalIncrements = 0;
+
+        for (int i = 0; i < _input.Length - 3; i++)
+        {
+            var sum = _input[i] + _input[i + 1] + _input[i + 2];
+
+            totalIncrements += sum > previousIncrement ? 1 : 0;
+            previousIncrement = sum;
+        }
+
+        return new(totalIncrements.ToString());
+    }
 }
---
+++ AdventOfCode/Day_02.cs
@@ -0,0 +1,68 @@
+﻿namespace AdventOfCode;
+
+internal class Day_02 : BaseDay
+{
+    private readonly string[] _input;
+
+    public Day_02()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var horizontal = 0;
+        var depth = 0;
+
+        foreach (var instruction in _input)
+        {
+            var data = instruction.Split();
+            var value = int.Parse(data[1]);
+
+            if (data[0] == "forward")
+            {
+                horizontal += value;
+            }
+            else if (data[0] == "down")
+            {
+                depth += value;
+            }
+            else
+            {
+                depth -= value;
+            }
+        }
+
+        return new($"{horizontal * depth}");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var horizontal = 0;
+        var depth = 0;
+        var aim = 0;
+
+        foreach (var instruction in _input)
+        {
+            var data = instruction.Split();
+            var value = int.Parse(data[1]);
+
+            if (data[0] == "forward")
+            {
+                horizontal += value;
+                depth += aim * value;
+            }
+            else if (data[0] == "down")
+            {
+                aim += value;
+            }
+            else
+            {
+                aim -= value;
+            }
+        }
+
+        return new($"{horizontal * depth}");
+    }
+}
+
