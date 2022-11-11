4b1936254024c05c9b840c964930e0a79ad81f2c
Sean Edwards <hello@seanedwards.dev>
2022-11-11 14:28:50 +0000

    Add solutions for day 8 and 9

---
+++ AdventOfCode/Days/Day_08.cs
@@ -0,0 +1,84 @@
+﻿namespace AdventOfCode.Days;
+
+internal class Day_08 : BaseDay
+{
+    private readonly string[] _signalPatterns;
+    private readonly string[] _outputValues;
+
+    public Day_08()
+    {
+        var input = File.ReadAllLines(InputFilePath);
+
+        _signalPatterns = new string[input.Length];
+        _outputValues = new string[input.Length];
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            var line = input[i];
+            var data = line.Split(" | ");
+
+            _signalPatterns[i] = data[0];
+            _outputValues[i] = data[1];
+        }
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        // Key: The number itself
+        // Value: Number of segments in a number
+        var numberSegments = new Dictionary<int, int>
+        {
+            { 1, 2 },
+            { 4, 4 },
+            { 7, 3 },
+            { 8, 7 }
+        };
+
+        var count = 0;
+
+        foreach (var digit in _outputValues)
+        {
+            count += digit.Split(" ").Where(x => numberSegments.ContainsValue(x.Length)).Count();
+        }
+
+        return new(count.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var total = 0;
+
+        for (var i = 0; i < _signalPatterns.Length; i++)
+        {
+            var signals = _signalPatterns[i].Split(" ").Select(x => string.Concat(x.OrderBy(i => i))).OrderBy(x => x.Length).ToArray();
+            var charactersToNumber = new Dictionary<string, int>
+            {
+                { signals[0], 1 },
+                { signals[1], 7 },
+                { signals[2], 4 },
+                { signals[9], 8 }
+            };
+
+            var six = signals.Where(x => x.Length == 6 && x.Intersect(signals[0]).Count() == 1).First();
+            charactersToNumber.Add(six, 6);
+            var three = signals.Where(x => x.Length == 5 && x.Intersect(signals[1]).Count() == 3).First();
+            charactersToNumber.Add(three, 3);
+
+            var fourAndThree = string.Concat(signals[2].Union(three));
+            charactersToNumber.Add(signals.Where(x => x.Length == 6 && x.Intersect(fourAndThree).Count() == 6).First(), 9);
+            charactersToNumber.Add(signals.Where(x => x.Length == 6 && !charactersToNumber.ContainsKey(x)).First(), 0);
+            charactersToNumber.Add(signals.Where(x => x.Intersect(six).Count() == 5).First(), 5);
+            charactersToNumber.Add(signals.Where(x => !charactersToNumber.ContainsKey(x)).First(), 2);
+
+            var number = string.Empty;
+            foreach (var output in _outputValues[i].Split(" ").Select(x => string.Concat(x.OrderBy(i => i))))
+            {
+                number = $"{number}{charactersToNumber[output]}";
+            }
+
+            total += int.Parse(number);
+        }
+
+        return new(total.ToString());
+    }
+}
---
+++ AdventOfCode/Days/Day_09.cs
@@ -0,0 +1,120 @@
+﻿namespace AdventOfCode.Days;
+
+internal class Day_09 : BaseDay
+{
+    private readonly int[][] _input;
+    private List<(int X, int Y)> _basins;
+    private bool[][] _areaInBasin;
+
+    public Day_09()
+    {
+        _input = File.ReadAllLines(InputFilePath).Select(x => x.ToCharArray().Select(x => x - '0').ToArray()).ToArray();
+        _basins = new List<(int X, int Y)>();
+        _areaInBasin = new bool[_input.GetLength(0)][];
+
+        for (int i = 0; i < _areaInBasin.Length; i++)
+        {
+            _areaInBasin[i] = new bool[_input[0].Length];
+        }
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var riskLevel = 0;
+
+        for (int i = 0; i < _input.Length; i++)
+        {
+            for (int j = 0; j < _input[0].Length; j++)
+            {
+                var neighbours = new List<int>();
+
+                if (i != 0)
+                {
+                    neighbours.Add(_input[i - 1][j]);
+                }
+
+                if (j != 0)
+                {
+                    neighbours.Add(_input[i][j - 1]);
+                }
+
+                if (i != _input.Length - 1)
+                {
+                    neighbours.Add(_input[i + 1][j]);
+                }
+
+                if (j != _input[0].Length - 1)
+                {
+                    neighbours.Add(_input[i][j + 1]);
+                }
+
+                if (neighbours.All(x => x > _input[i][j]))
+                {
+                    riskLevel += _input[i][j] + 1;
+                    _basins.Add((i, j));
+                }
+            }
+        }
+
+        return new(riskLevel.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var largestBasins = new List<int>();
+
+        foreach (var basin in _basins)
+        {
+            var basinSize = 1;
+
+            basinSize = Foo(basin.X, basin.Y, basinSize);
+
+            largestBasins.Add(basinSize);
+        }
+
+        return new(largestBasins.OrderByDescending(x => x).Take(3).Aggregate((x, y) => x * y).ToString());
+    }
+
+    private int Foo(int x, int y, int basinSize)
+    {
+
+        if (_input[x][y] == 9 || _areaInBasin[x][y])
+        {
+            return 0;
+        }
+
+        _areaInBasin[x][y] = true;
+
+        // Up
+        if (IsValidCoordinate(x - 1, y))
+        {
+            basinSize += Foo(x - 1, y, 1);
+        }
+
+        // Left
+        if (IsValidCoordinate(x, y - 1))
+        {
+            basinSize += Foo(x, y - 1, 1);
+        }
+
+        // Right
+        if (IsValidCoordinate(x, y + 1))
+        {
+            basinSize += Foo(x, y + 1, 1);
+        }
+
+        // Down
+        if (IsValidCoordinate(x + 1, y))
+        {
+            basinSize += Foo(x + 1, y, 1);
+        }
+
+        return basinSize;
+
+    }
+
+    private bool IsValidCoordinate(int x, int y)
+    {
+        return !(x < 0 || y < 0 || x > _input.Length - 1 || y > _input[0].Length - 1);
+    }
+}
